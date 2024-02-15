2f56b0de8b30b693dc00aaefbdf0fe9f5eab5482
artl <33ttpp@gmail.com>
2024-02-15 21:05:59 +0100

    added parametrs in TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,20 +1,50 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 
 public class TestBase {
 
-    @BeforeAll static void beforAll() {
-        Configuration.baseUrl = "https://www.aerticket.de/";
-        Configuration.browserSize = "1920x1080";
+    @BeforeAll
+    static void beforAll() {
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://www.aerticket.de/");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "110.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("wdHost", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000;
-        Configuration.holdBrowserOpen = true;
 
-     }
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachmentsAndCloseWebDriver() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
+    }
 
 
 }
+
+
+
+
